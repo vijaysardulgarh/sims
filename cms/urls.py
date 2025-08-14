a40f22a0ebf8d7d60ab982194b77079366caae00
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-14 16:38:19 +0530

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -3,12 +3,13 @@ from . import views
 from .views import timetable_view
 urlpatterns = [
     path('',views.index,name='index'),
-    path('about',views.about,name='about.html'),
-    path('contact',views.about,name='contact.html'),
-    path('staff_members',views.staff,name='staff_members.html'),
+    path('about',views.about,name='about'),
+    path('contact',views.about,name='contact'),
+    path('staff_members',views.staff,name='staff_members'),
     path('student_strength',views.student_strength,name='student_strength'),
     path('subject_strength',views.subject_strength,name='subject_strength'),
-    #path('school_subject_strength',views.student_strength,name='school_subject_strength.html'),
+    #path('school_subject_strength',views.student_strength,name='school_subject_strength'),
     path('timetable/', timetable_view, name='timetable'),
   
 ]
+
