3428ed54c26673242b0ae69b28ea20d16f0d281e
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-17 16:27:26 +0530

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
     path('contact',views.about,name='contact'),
-    path('staff_members',views.staff,name='staff_members'),
+    #path('staff_members',views.staff,name='staff_members'),
     path('student_strength',views.student_strength,name='student_strength'),
     path('subject_strength',views.subject_strength,name='subject_strength'),
     #path('school_subject_strength',views.student_strength,name='school_subject_strength'),
@@ -13,5 +13,6 @@ urlpatterns = [
     path("staff/<str:role>/", views.staff_by_role, name="staff_by_role"),
   
 ]
+
 
 
