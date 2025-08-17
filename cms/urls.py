783a163d45f4e70509cee67c532a59a08be8142a
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-17 20:48:50 +0530

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 from .views import timetable_view
+from django.conf import settings
+from django.conf.urls.static import static
+import os
 urlpatterns = [
+    path("school/<int:school_id>/documents/", views.document_detail, name="document_detail"),
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
     path('contact',views.about,name='contact'),
@@ -16,3 +20,5 @@ urlpatterns = [
 
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
