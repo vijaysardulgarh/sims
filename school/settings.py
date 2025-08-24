7811c0a87aab1367c98ec1231d9ad3f3c13ac8df
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-24 09:34:47 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')  # Define a separate directory for collected static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Define a separate directory for collected static files.
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')  # Specify additional directories for static files.
@@ -133,7 +133,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
