7f52b04a1e364e6f99b2423c2c4d4b5173ee6eb9
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-29 13:24:48 +0530

    Update models.py

--- cms/models.py
+++ cms/models.py
@@ -923,20 +923,19 @@ class ExamDetail(models.Model):
 
 class FeeStructure(models.Model):
     student_class = models.ForeignKey("Class", on_delete=models.CASCADE)  
+    stream = models.ForeignKey("Stream", on_delete=models.CASCADE, null=True, blank=True)  
+
     admission_fee = models.DecimalField(max_digits=10, decimal_places=2)
     rcf = models.DecimalField(max_digits=10, decimal_places=2)  
     cwf = models.DecimalField(max_digits=10, decimal_places=2)   
     ccwf = models.DecimalField(max_digits=10, decimal_places=2)  
 
     def total_fee(self):
-        return (
-            self.admission_fee 
-            + self.rcf 
-            + self.cwf 
-            + self.ccwf
-        )
+        return self.admission_fee + self.rcf + self.cwf + self.ccwf
 
     def __str__(self):
+        if self.stream:
+            return f"Fee Structure for {self.student_class} ({self.stream})"
         return f"Fee Structure for {self.student_class}"
         
 
@@ -968,4 +967,5 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+
 
