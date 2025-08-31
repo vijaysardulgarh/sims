9026eb13d693eadb42a296911ea14657d63358cf
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2025-08-31 10:44:12 +0530

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -1,31 +1,26 @@
 from django.urls import path
-from . import views
-from .views import timetable_view
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+from .views import timetable_view
 
 urlpatterns = [
-    path("school/<int:school_id>/documents/", views.document_detail, name="document_detail"),
-
+    path('login/', views.user_login, name='login'),
     # Home
     path('', views.index, name='index'),
 
-    # About Us
+    # About
     path('about/', views.about_school, name='about'),
     path('principal/', views.principal, name='principal_message'),
     path('committee/', views.committee, name='committee'),
-    path("nodal/", views.nodal, name="nodal"),
-    #path('nodal', views.nodal, name='nodal_detail'),
-    #path("nodals/", views.nodal_list, name="nodal_list"),
-    # path("nodals/<int:pk>/", views.nodal_detail, name="nodal_detail"),
+    path('nodal/', views.nodal, name='nodal'),
     path('affiliation/', views.affiliation_status, name='affiliation_status'),
-    path("infrastructure/", views.infrastructure, name="infrastructure"),
-    #path('infrastructure/', views.infrastructure, name='infrastructure'),
+    path('infrastructure/', views.infrastructure, name='infrastructure'),
     path('smc_members/', views.smc_members, name='smc_members'),
-    path("staff/associations/", views.staff_association_roles, name="staff_association_roles"),
+    path('staff/associations/', views.staff_association_roles, name='staff_association_roles'),
 
     # Academics
-    path("subjects_offered/", views.subjects_offered, name="subjects"),
+    path('subjects_offered/', views.subjects_offered, name='subjects'),
     path('curriculum/', views.curriculum, name='curriculum'),
     path('academics/syllabus/', views.syllabus, name='syllabus'),
     path('academics/exams/', views.exams_results, name='exams'),
@@ -50,40 +45,38 @@ urlpatterns = [
     path('subject_strength/', views.subject_strength, name='subject_strength'),
     path('school_subject_strength/', views.subject_strength, name='school_subject_strength'),
 
-
     # Timetable
     path('timetable/', timetable_view, name='timetable'),
 
     # Students
-    path("achievements/", views.achievement_list, name="achievement_list"),
-    path("achievements/<int:pk>/", views.achievement_detail, name="achievement_detail"),
+    path('achievements/', views.achievement_list, name='achievement_list'),
+    path('achievements/<int:pk>/', views.achievement_detail, name='achievement_detail'),
     path('students/achievements/', views.achievements, name='achievements'),
     path('students/results/', views.board_results, name='board_results'),
     path('students/sports/', views.sports_achievements, name='sports_achievements'),
     path('students/gallery/', views.gallery, name='gallery'),
     path('students/notices/', views.notices, name='notices'),
     path('students/events/', views.events, name='events'),
+    path('signin/<str:class_name>/<str:section_name>/', views.signin, name='signin'),
+    path('roll-call/<str:class_name>/<str:section_name>/', views.roll_call, name='roll_call'),
+    path('signin_link/', views.signin_link, name='signin_link'),
+    path('roll_call_link/', views.roll_call_link, name='roll_call_link'),
+    path('student-strength-pdf/', views.generate_student_strength_pdf, name='student_strength_pdf'),
 
-    # Admissions
+    # Admissions & Fees
     path('admission_procedure/', views.admission_procedure, name='admission_procedure'),
     path('admission_form/', views.admission_form, name='admission_form'),
     path('admissions/prospectus/', views.prospectus, name='prospectus'),
-    path("fees/", views.fee_structure_list, name="fee_structure_list"),
-    #path('fee_structure/', views.fee_structure, name='fee_structure'),
-    path('faq', views.faq, name='faq'),
+    path('fees/', views.fee_structure_list, name='fee_structure_list'),
 
-    # Mandatory Info
-    path('mandatory/disclosure/', views.mandatory_disclosure, name='mandatory_disclosure'),
-    path("committee/<int:pk>/", views.committee_detail, name="committee_detail"),
-    # path('mandatory/statistics/', views.statistics, name='statistics'),
-    # path('mandatory/safety/', views.safety_committee, name='safety_committee'),
-    # path('mandatory/grievance/', views.grievance_committee, name='grievance_committee'),
-    # path('mandatory/icc/', views.icc_committee, name='icc_committee'),
-
-    # Contact
+    # FAQ & Contact
+    path('faq/', views.faq, name='faq'),
+    path('mandatory_public_disclosure/', views.mandatory_public_disclosure, name='mandatory_public_disclosure'),
     path('contact/', views.contact, name='contact'),
+
+    # School Documents
+    path('school/<int:school_id>/documents/', views.document_detail, name='document_detail'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
