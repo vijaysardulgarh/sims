c986991f5d03572c569e5e6086b20fd9650e4ebf
Vijay Kumar <vijaysardulgarh@gmail.com>
2026-05-05 10:43:59 +0530

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for sims project.
+URL configuration for api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
